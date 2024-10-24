f26ccea90b33a32bc6602e94404c8719c7616d2e
Zoofear <misterz0312@gmail.com>
2024-10-24 19:02:22 -0400

    Finished the counterType testing

--- counterType/counterTypeTest.cpp
+++ counterType/counterTypeTest.cpp
@@ -15,6 +15,32 @@ int main()
     cout << "outputting the value of the default constructor" << endl;
     defaultcounter.print();
 
-    cout << "outputting the value of the constructer with a parameter" << endl;
+    cout << "outputting the value of the constructer with a parameter of 5" << endl;
     parametercounter.print();
+
+    cout << "showing the ability for the counter to increment and decrement" << endl;
+
+    parametercounter.print();
+    parametercounter.incrementCounter();
+    parametercounter.print();
+    parametercounter.decrementCounter();
+    parametercounter.print();
+
+    cout << "showwing the ability for the counter to be reset" << endl;
+
+    parametercounter.print();
+    parametercounter.reset();
+    parametercounter.print();
+
+    cout << "showing the ability for the counter to recognize it is going into a negative integer" << endl;
+
+    parametercounter.print();
+    parametercounter.decrementCounter();
+
+    cout << "showing the ability for the counter to be set to a specific integer" << endl;
+    defaultcounter.setCounter(10);
+    defaultcounter.print();
+
+    cout << "showing the ability for the counter to recognize when it is trying to be set to a negative integer" << endl;
+    defaultcounter.setCounter(-1);
 }
